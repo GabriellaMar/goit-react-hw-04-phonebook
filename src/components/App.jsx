43128ac968bccfa13529f91f6c2e85055108a4cb
Gabriella Marusyak <slipuhinag@gmail.com>
2023-07-04 17:05:42 +0300

    Did task 4(phonebook)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
+
+import { ContactForm } from './ContactForm/ContactForm'
+import { nanoid } from 'nanoid'
+import { Filter } from "./Filter/Filter";
+import { ContactList } from "./ContactList/ContactList";
+import { useState, useEffect } from 'react';
+
+
+const useLocalStorage = (key, defaultValue) => {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(contacts));
+  }, [key, contacts]);
+
+  return [contacts, setContacts];
+}
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+  const [contacts, setContacts] = useLocalStorage('contacts', []);
+  const [filter, setFilter] = useState('');
+
+  const addUserContact = ({ name, number }) => {
+    const userContact = {
+      name,
+      number,
+      id: nanoid()
+    }
+
+    setContacts(prevState => [...prevState, userContact],
+    );
+  }
+
+
+  const deleteUserContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId))
+  }
+
+  const handleFilterChange = (e) => {
+    const { value } = e.target
+    setFilter(value)
+  }
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name && contact.name.toLowerCase().includes(filter.toLowerCase()))
+
+  return <div>
+    <h1>Phonebook</h1>
+
+    <ContactForm contacts={contacts} addUserContact={addUserContact} />
+
+    <h1>Contacts</h1>
+
+    <Filter filter={filter} onFilterChange={handleFilterChange} />
+
+    <ContactList filteredContacts={filteredContacts} onDeleteContact={deleteUserContact} />
+  </div>
+}
+
+
